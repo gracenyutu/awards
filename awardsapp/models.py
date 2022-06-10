e2c1492517152be71c9f42a8044261938afff121
Grace Nyutu <graceynyutu@gmail.com>
2022-06-10 22:02:37 +0300

    register models in admin

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -16,6 +16,6 @@ class Post(models.Model):
     url = models.URLField(max_length=255)
     description = models.TextField(max_length=255)
     technologies = models.CharField(max_length=200, blank=True)
-    photo = ImageField(manual_crop='1280x720')
+    photo = ImageField(manual_crop='1280x720',upload_to='posts')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     date = models.DateTimeField(auto_now_add=True, blank=True)
