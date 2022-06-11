00d6352b8ee1df2a35b0c4a94230905d522e0877
Grace Nyutu <graceynyutu@gmail.com>
2022-06-11 12:21:57 +0300

    create home and login views

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    if not request.user.is_authenticated:
+        return redirect("Login")
+    return render(request, 'awwards/home.html')
+
+def Login(request):
+    return render(request, 'registration/login.html')
