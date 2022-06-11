cd9d5aeea0855f7358b23b6b4dbb4eb1d48bcb68
Grace Nyutu <graceynyutu@gmail.com>
2022-06-11 12:51:31 +0300

    create signup view

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .forms import SignupForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def home(request):
@@ -8,3 +10,17 @@ def home(request):
 
 def Login(request):
     return render(request, 'registration/login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
