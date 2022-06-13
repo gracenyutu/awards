e16945564ee54945cb7e89d961cdb9d55817cfb8
Grace Nyutu <graceynyutu@gmail.com>
2022-06-13 14:09:34 +0300

    create api folder

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,4 +1,3 @@
-from telnetlib import LOGOUT
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SignupForm, RatingForm, PostForm, UpdateUserForm, UpdateUserProfileForm
 from django.contrib.auth import login, authenticate
